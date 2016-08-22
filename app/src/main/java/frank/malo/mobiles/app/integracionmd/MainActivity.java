581af55039422682da8fc0eeeb7f5705c32f24d1
frankmalo86 <frankmalo86@gmail.com>
2016-08-22 13:44:43 -0500

    inicio

---
+++ app/src/main/java/frank/malo/mobiles/app/integracionmd/MainActivity.java
@@ -0,0 +1,13 @@
+package frank.malo.mobiles.app.integracionmd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
